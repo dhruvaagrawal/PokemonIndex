e3a8ccb81f6a8d2dd683d03b772360343f373017
Dhruva Agrawal <dhruva1998@gmail.com>
2023-07-07 01:26:29 +0530

    Added Auth UI

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -14,23 +14,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-5 lg:px-0">
         <Link
           href="/examples/authentication"
           className={cn(
@@ -40,35 +24,40 @@ export default function AuthenticationPage() {
         >
           Login
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
+        <div className="relative hidden h-screen flex-col p-10 text-white dark:border-r lg:flex lg:col-span-3">
+          <div className="absolute inset-0">
+            {" "}
+            <img
+              src="/assets/hero.jpg"
+              alt="Pokemon Hero"
+              className="block h-screen object-cover object-center"
+            />
+            {/* This is the element we add for creating the overlay: */}
+            <div className="absolute inset-0 bg-black bg-opacity-70"></div>
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;
+                <a
+                  href="https://unsplash.com/photos/I7ipAK_JggQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  className="underline"
+                >
+                  Photo
+                </a>{" "}
+                by{" "}
+                <a
+                  href="https://unsplash.com/de/@giorgiotrovato?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  className="underline"
+                >
+                  Giorgio Trovato
+                </a>{" "}
+                on Unsplash &rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 lg:col-span-2">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
