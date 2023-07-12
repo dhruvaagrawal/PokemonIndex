9e55ffccd47aee2aea7d43418ac542e7a4d224b2
Dhruva Agrawal <dhruva1998@gmail.com>
2023-07-13 03:22:15 +0530

    Added Badges row

--- components/users/ProfileOverview.tsx
+++ components/users/ProfileOverview.tsx
@@ -3,8 +3,27 @@ import Image from "next/image"
 import { Balancer } from "react-wrap-balancer"
 
 import { cn } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
+import { Card } from "@/components/ui/card"
 
-import { Badge } from "../ui/badge"
+const dummyBadgesArray = [
+  {
+    name: "Bronze Badge",
+    description: "Achieved bronze level",
+    image: "/assets/bronze-medal.png",
+  },
+  {
+    name: "Silver Badge",
+    description: "Achieved silver level",
+    image: "/assets/silver-medal.png",
+  },
+  {
+    name: "Gold Badge",
+    description: "Achieved gold level",
+    image: "/assets/gold-medal.png",
+  },
+  // Add more dummy badge objects as needed
+]
 
 interface ProfileOverviewProps extends HTMLAttributes<HTMLDivElement> {}
 
@@ -26,10 +45,10 @@ const ProfileOverview: FC<ProfileOverviewProps> = ({ className }) => {
         />
       </div>
       <div className="lg:col-span-4">
-        <div className="grid grid-cols-2">
-          <div className="flex col-span-2 justify-center space-x-4 my-2">
+        <div className="grid grid-cols-2 mt-2 gap-4">
+          <div className="flex col-span-2 justify-center space-x-4">
             <div className="text-center">
-              <h1 className="text-4xl font-semibold">Dhruva's Overview</h1>
+              <h1 className="text-5xl font-semibold">Dhruva's Overview</h1>
             </div>
             <div className="space-x-2 flex items-center">
               <Badge variant="outline">Google</Badge>
@@ -38,11 +57,20 @@ const ProfileOverview: FC<ProfileOverviewProps> = ({ className }) => {
             </div>
           </div>
           <div className="col-span-2 p-2 ml-2 text-center rounded-md border dark:border-slate-800 dark:bg-background dark:text-slate-50">
-            <blockquote className="italic text-sm text-gray-500">
-              &#8220; just a guy, coding his way, through this messed up world,
-              one incorrectly labelled variable at a time.&#8221;
+            <blockquote className="italic text-md text-gray-500">
+              <Balancer>
+                &#8220; just a guy, coding his way, through this messed up
+                world, one incorrectly labelled variable at a time.&#8221;
+              </Balancer>
             </blockquote>
           </div>
+          <Card className="flex flex-grow">
+            {dummyBadgesArray.map(({ name, image }) => (
+              <div className="rounded-full">
+                <Image src={image} alt={name} width={40} height={40} />
+              </div>
+            ))}
+          </Card>
         </div>
       </div>
     </div>
