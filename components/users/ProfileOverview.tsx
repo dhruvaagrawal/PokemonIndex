71d68c85520ad15b5e573ede60d4d31151c79cdb
Dhruva Agrawal <dhruva1998@gmail.com>
2023-07-13 02:14:58 +0530

    Making changes to the ProfileOverview component - Finished Name + Badges row

--- components/users/ProfileOverview.tsx
+++ components/users/ProfileOverview.tsx
@@ -11,29 +11,29 @@ const ProfileOverview: FC<ProfileOverviewProps> = ({ className }) => {
   return (
     <div
       className={cn(
-        "rounded-lg border border-slate-200 bg-white text-slate-950 shadow-sm dark:border-slate-800 dark:bg-background dark:text-slate-50 flex flex-col",
+        "rounded-lg border border-slate-200 bg-white text-slate-950 shadow-sm dark:border-slate-800 dark:bg-background dark:text-slate-50 grid grid-cols-5 p-4",
         className
       )}
     >
-      <div>
+      <div className="lg:col-span-1">
         <Image
           src="/assets/user.jpg"
           alt="User"
-          height={300}
+          height={200}
           width={200}
-          className="rounded-lg"
+          className="rounded-lg shadow-2xl"
         />
       </div>
-      <div>
-        <div className="flex">
-          <div>
-            <div>
-              <h1>Dhruva's Overview</h1>
+      <div className="lg:col-span-4 my-2">
+        <div className="grid grid-cols-2">
+          <div className="grid col-span-2 grid-cols-5">
+            <div className="lg:col-span-3 text-center">
+              <h1 className="text-5xl font-semibold">Dhruva's Overview</h1>
             </div>
-            <div>
-              <Badge>Google</Badge>
-              <Badge>Github</Badge>
-              <Badge>Twitter</Badge>
+            <div className="lg:col-span-2 space-x-2 flex items-center">
+              <Badge variant="outline">Google</Badge>
+              <Badge variant="outline">Github</Badge>
+              <Badge variant="outline">Twitter</Badge>
             </div>
           </div>
         </div>
