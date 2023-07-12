66bdfe170ab9bd7b68eee22447c2da2686c4c584
Dhruva Agrawal <dhruva1998@gmail.com>
2023-07-13 03:25:44 +0530

    Completed the ProfileOverview component

--- components/users/ProfileOverview.tsx
+++ components/users/ProfileOverview.tsx
@@ -44,34 +44,36 @@ const ProfileOverview: FC<ProfileOverviewProps> = ({ className }) => {
           className="rounded-lg shadow-2xl"
         />
       </div>
-      <div className="lg:col-span-4">
-        <div className="grid grid-cols-2 mt-2 gap-4">
-          <div className="flex col-span-2 justify-center space-x-4">
-            <div className="text-center">
-              <h1 className="text-5xl font-semibold">Dhruva's Overview</h1>
-            </div>
-            <div className="space-x-2 flex items-center">
-              <Badge variant="outline">Google</Badge>
-              <Badge variant="outline">Github</Badge>
-              <Badge variant="outline">Twitter</Badge>
-            </div>
-          </div>
-          <div className="col-span-2 p-2 ml-2 text-center rounded-md border dark:border-slate-800 dark:bg-background dark:text-slate-50">
-            <blockquote className="italic text-md text-gray-500">
-              <Balancer>
-                &#8220; just a guy, coding his way, through this messed up
-                world, one incorrectly labelled variable at a time.&#8221;
-              </Balancer>
-            </blockquote>
-          </div>
-          <Card className="flex flex-grow">
-            {dummyBadgesArray.map(({ name, image }) => (
-              <div className="rounded-full">
-                <Image src={image} alt={name} width={40} height={40} />
+      <div className="lg:col-span-4 flex flex-col">
+        <div className="flex flex-grow-0">
+          <div className="grid grid-cols-2 my-2 gap-4">
+            <div className="flex col-span-2 justify-center space-x-4">
+              <div className="text-center">
+                <h1 className="text-5xl font-semibold">Dhruva's Overview</h1>
               </div>
-            ))}
-          </Card>
+              <div className="space-x-2 flex items-center">
+                <Badge variant="outline">Google</Badge>
+                <Badge variant="outline">Github</Badge>
+                <Badge variant="outline">Twitter</Badge>
+              </div>
+            </div>
+            <div className="col-span-2 p-2 ml-2 text-center rounded-md border dark:border-slate-800 dark:bg-background dark:text-slate-50">
+              <blockquote className="italic text-md text-gray-500">
+                <Balancer>
+                  &#8220; just a guy, coding his way, through this messed up
+                  world, one incorrectly labelled variable at a time.&#8221;
+                </Balancer>
+              </blockquote>
+            </div>
+          </div>
         </div>
+        <Card className="flex flex-grow overflow-auto">
+          {dummyBadgesArray.map(({ name, image }) => (
+            <div className="rounded-full">
+              <Image src={image} alt={name} width={40} height={40} />
+            </div>
+          ))}
+        </Card>
       </div>
     </div>
   )
